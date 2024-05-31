e740396e289c4b0b6b717770f408562a1ce66aae
Damien Marill <dmarill@kalya-sante.com>
2024-05-31 14:02:29 +0200

    fix: environment prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {};
 
-export const BACKEND_URL = 'http://localhost:8000';
+export const BACKEND_URL = 'https://api.15.mmi3.iutbeziers.fr';
 export const BACKEND_URL_API = BACKEND_URL + '/api';
 
 export const MIX_PUSHER_APP_KEY = "TestWebsocket";
-export const MIX_PUSHER_HOST = 'localhost';
+export const MIX_PUSHER_HOST = 'api.15.mmi3.iutbeziers.fr';
 export const MIX_PUSHER_PORT = 6001;
 export const MIX_PUSHER_PORT_TLS = 6001;
-export const MIX_PUSHER_FORCE_TLS = false;
-export const MIX_PUSHER_TRANSPORT = ['ws'];
+export const MIX_PUSHER_FORCE_TLS = true;
+export const MIX_PUSHER_TRANSPORT = ['wss'];
